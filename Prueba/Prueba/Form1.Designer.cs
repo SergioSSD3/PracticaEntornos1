5ac5de25b4dc2f4409dbb11a0b668dfa07c09184
Sergio <sergioseguradura@gmail.com>
2022-05-11 20:07:11 +0200

    prueba2

--- Prueba/Prueba/Form1.Designer.cs
+++ Prueba/Prueba/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.boxdos = new System.Windows.Forms.TextBox();
+            this.buno = new System.Windows.Forms.Button();
+            this.boxuno = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // boxdos
+            // 
+            this.boxdos.Location = new System.Drawing.Point(299, 185);
+            this.boxdos.Name = "boxdos";
+            this.boxdos.Size = new System.Drawing.Size(202, 22);
+            this.boxdos.TabIndex = 5;
+            // 
+            // buno
+            // 
+            this.buno.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.buno.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buno.Location = new System.Drawing.Point(299, 230);
+            this.buno.Name = "buno";
+            this.buno.Size = new System.Drawing.Size(202, 76);
+            this.buno.TabIndex = 4;
+            this.buno.Text = "Calcular:";
+            this.buno.UseVisualStyleBackColor = false;
+            this.buno.Click += new System.EventHandler(this.buno_Click);
+            // 
+            // boxuno
+            // 
+            this.boxuno.Location = new System.Drawing.Point(299, 145);
+            this.boxuno.Name = "boxuno";
+            this.boxuno.Size = new System.Drawing.Size(202, 22);
+            this.boxuno.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.boxdos);
+            this.Controls.Add(this.buno);
+            this.Controls.Add(this.boxuno);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox boxdos;
+        private System.Windows.Forms.Button buno;
+        private System.Windows.Forms.TextBox boxuno;
     }
 }
 
--- Prueba/Prueba/Form1.cs
+++ Prueba/Prueba/Form1.cs
@@ -16,5 +16,70 @@ namespace Prueba
         {
             InitializeComponent();
         }
+
+        double factorial(int numero1)
+        {
+            int factor = 1;
+
+            for (int i = 1; i <= numero1; i++)
+            {
+
+                factor = factor * i;
+
+            }
+
+            return factor;
+        }
+
+        double potencias(int factor, int numeroM)
+        {
+
+            int poten = 1;
+
+            for (int i = 1; i <= factor; i++)
+            {
+
+                poten = poten * numeroM;
+
+            }
+            return poten;
+        }
+
+        double series(int numeroM, int numeroN)
+        {
+            double result = 0;
+
+            double resultadopotencia, resultadofactorial;
+
+            for (int i = 1; i <= numeroM; i++)
+            {
+
+                resultadopotencia = potencias(i, numeroM);
+
+                resultadofactorial = factorial(i);
+
+                result = result + resultadopotencia / resultadofactorial;
+
+            }
+
+            return result;
+        }
+
+        private void buno_Click(object sender, EventArgs e)
+        {
+            int numeroM, numeroN;
+
+            double definitivo;
+
+
+            numeroM = int.Parse(boxuno.Text);
+
+            numeroN = int.Parse(boxdos.Text);
+
+
+            definitivo = series(numeroM, numeroN);
+
+            MessageBox.Show("La serie de los numeros esritos es: " + definitivo);
+        }
     }
-}
+    }
