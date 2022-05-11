5513b01c9161add8624bcb94b236c3ff045efa19
Sergio <sergioseguradura@gmail.com>
2022-05-11 20:12:33 +0200

    prueba2

--- Prueba/Prueba/Form1.Designer.cs
+++ Prueba/Prueba/Form1.Designer.cs
@@ -69,7 +69,6 @@
             this.Controls.Add(this.boxuno);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
